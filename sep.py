127fe228d98f6b6823eb21ff7331da34494bf17d
Srabonti Sen Neeti <105552240+SrabontiSenNeeti@users.noreply.github.com>
2022-06-17 14:25:41 +0600

    Create sep.py

---
+++ sep.py
@@ -0,0 +1,18 @@
+
+print('a','b','c','d','e',sep=" ")
+
+print("Hello "," World ", " Neeti",sep="and")
+
+#For Formatting a date
+print("17","06","2022",sep="-")
+
+#EmailFormating
+print("srabonti.s2n","gmail.com",sep="@")
+
+print('a','s','d','f',sep=" ",end=" ")
+print("G")
+
+print("17","06","2022",sep="/",end="\n")
+
+print("neeti","sen",sep="",end="@")
+print("gmail.com")
